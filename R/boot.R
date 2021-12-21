31dcac82723f75587e0169fff2e6bcb7e48989bd
Hannes Riebl <hriebl@uni-goettingen.de>
2021-12-21 13:41:05 +0100

    Rename num_samples argument

--- R/boot.R
+++ R/boot.R
@@ -4,19 +4,19 @@
 #' from the [lmls()] function.
 #'
 #' @param m A location-scale regression model from the [lmls()] function.
-#' @param nsim The number of bootstrap samples to draw.
+#' @param num_samples The number of bootstrap samples to draw.
 #' @param seed Either `NULL` or an integer that will be used in a call to
 #'             [set.seed()] before simulating the response vectors.
 #'
 #' @importFrom stats coef simulate
 #' @export
 
-boot <- function(m, nsim = 1000, seed = NULL) {
+boot <- function(m, num_samples = 1000, seed = NULL) {
   if (m$light) {
     stop("Cannot bootstrap, lmls() called with argument 'light = TRUE'")
   }
 
-  sims <- simulate(m, nsim, seed)
+  sims <- simulate(m, num_samples, seed)
 
   coefs <- lapply(sims, function(y) {
     coef(lmls(y ~ 0 + m$x, ~ 0 + m$z))
